c3c2b2d5be3cfe659f7fe93bd66c920619a7c2ba
bbird5063 <bbird5063@gmail.com>
2024-10-04 19:09:41 +0300

    37. JWT Token в Laravel

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
+});
+
+/*
+Route::group([
+
+	'middleware' => 'api',
+	'prefix' => 'auth'
+
+], function ($router) {
+
+	Route::post('login', 'App\Http\Controllers\AuthController@login');
+	Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+	Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+	Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+});
+*/
+
+Route::group([
+	'namespace' => 'App\Http\Controllers',
+	'middleware' => 'api',
+	'prefix' => 'auth'
+], function ($router) {
+	Route::post('login', 'AuthController@login');
+	Route::post('logout', 'AuthController@logout');
+	Route::post('refresh', 'AuthController@refresh');
+	Route::post('me', 'AuthController@me');
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Post', 'middleware' => 'jwt.auth'], function () {
+	Route::get('/posts', 'IndexController');
 });
